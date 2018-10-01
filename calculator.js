abbed893071c75b9b026e2d9f01a1ee2cba3838d
hbishira12 <43544999+hbishira12@users.noreply.github.com>
2018-10-01 22:15:22 +0100

    added

---
+++ calculator.js
@@ -0,0 +1,41 @@
+
+    // get the user inputs
+    var value1 = prompt('enter first value');
+    var value2 = prompt('enter the second value');
+    var choice = prompt('Choose your action (a)dd (s)ubtract (m)ultiply (d)ivide') || "a";
+     var total;
+  //  alert(var sum = value1 + choice + value2) ;
+
+/*
+  var x,y,z,oper;
+  x=prompt("enter first value");
+  y=prompt("enter second value");
+  oper=prompt("Type an operator");
+*/
+  if(choice=="+"){
+    total= (value1)+(+value2);
+  }
+  else if(choice=="/"){
+    total=(+value1)/(+value2);
+  }
+  else if(choice=="*"){
+    total=(+value1)*(+value2);
+  }
+  else if(choice=="-"){
+    total=(+value1)-(+value2);
+  }
+  else {
+    total=(+value1)+(+value2);
+  }
+/*
+  else if(choice=!null){
+    z="Error"
+  }
+  else if(value1=!null){
+    z="Error"
+  }
+  else if(value2=!null){
+    z="Error"
+  }
+*/
+  prompt(total);
